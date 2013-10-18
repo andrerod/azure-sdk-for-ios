8b4fb36d774e3493c55cfc20c030df2346e3fe60
Andre Rodrigues <andrerod@microsoft.com>
2013-10-18 18:15:05 +0100

    Add websiteclient

---
+++ azure/MSWebSiteManagementClient.h
@@ -0,0 +1,15 @@
+//
+//  MSWebSiteManagementClient.h
+//  azure
+//
+//  Created by André Rodrigues on 10/18/13.
+//  Copyright (c) 2013 Microsoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MSWebSiteManagementClient : NSObject
+
+- (void)listWebSites;
+
+@end
---
+++ azure/MSWebSiteManagementClient.m
@@ -0,0 +1,13 @@
+//
+//  MSWebSiteManagementClient.m
+//  azure
+//
+//  Created by André Rodrigues on 10/18/13.
+//  Copyright (c) 2013 Microsoft. All rights reserved.
+//
+
+#import "MSWebSiteManagementClient.h"
+
+@implementation MSWebSiteManagementClient
+
+@end
